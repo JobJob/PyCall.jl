82f1a648b786b7301df76adcf8a084e2b6eb4d51
Joel Mason <jobba1@hotmail.com>
2018-04-13 18:15:20 +1000

    rename to pycalls.jl

--- src/PyCall.jl
+++ src/PyCall.jl
@@ -691,7 +691,7 @@ function pybuiltin(name)
 end
 
 #########################################################################
-include("pyfuncwrap.jl")
+include("pycalls.jl")
 
 """
 Low-level version of `pycall(o, ...)` that always returns `PyObject`.
